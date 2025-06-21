82c81c08ee41abda0e223ce777840e2928d78dcf
ymiguell <miguelmelo.o.fut@gmail.com>
2025-06-21 15:00:30 -0300

    Implementação da camada de domínio

---
+++ src/main/java/me/dio/domain/model/Card.java
@@ -0,0 +1,42 @@
+package me.dio.domain.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity(name = "tb_card")
+public class Card {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String cardNumber;
+
+    @Column(name = "available_limit", scale = 13, precision = 2)
+    private BigDecimal limit;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public BigDecimal getLimit() {
+        return limit;
+    }
+
+    public void setLimit(BigDecimal limit) {
+        this.limit = limit;
+    }
+}
